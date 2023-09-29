ac939d229afe0543c0345dbd302dda69d7736ce4
Abhishek Kumar <abhi2004shek.kumar@gmail.com>
2023-09-29 15:50:35 +0530

    fixed client render

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import getCurrentUser from "@/actions/getCurrentUser";
 import getListings, { IListingsParams } from "@/actions/getListings";
+import ClientOnly from "@/components/clientOnly";
 import Container from "@/components/container";
 import EmptyState from "@/components/emptyState";
 import ListingCard from "@/components/listings/listingCard";
@@ -15,7 +16,7 @@ const Home = async ({ searchParams }: HomeProps) => {
     return <EmptyState showReset />;
   }
   return (
-    <div>
+    <ClientOnly>
       <Container>
         <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
           {listings.map((listing: any) => {
@@ -29,7 +30,7 @@ const Home = async ({ searchParams }: HomeProps) => {
           })}
         </div>
       </Container>
-    </div>
+    </ClientOnly>
   );
 };
 
